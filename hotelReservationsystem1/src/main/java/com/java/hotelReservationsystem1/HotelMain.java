369d95c2489dd3f41ed3469b855db7da29121b97
shubhammane2082 <shubhammane2082@gmail.com>
2024-03-27 15:54:24 +0530

    Find cheapest hotel in system

--- hotelReservationsystem1/src/main/java/com/java/hotelReservationsystem1/HotelMain.java
+++ hotelReservationsystem1/src/main/java/com/java/hotelReservationsystem1/HotelMain.java
@@ -1,5 +1,8 @@
 package com.java.hotelReservationsystem1;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,11 +15,11 @@ public class HotelMain {
 	public static void main(String[] args) 
 	{
 		HotelMain hotelmain=new HotelMain();
-		System.out.println("Welcome to Hotel Reservation System...");
+		System.out.println("Welcome to Hotel Reservation System..."+"\n");
 	     int choice;
 	     
 	     do {
-	    	 System.out.println("0.Exit 1.add Hotel");
+	    	 System.out.println("0.Exit 1.add Hotel 2.cheapest hotel rate for time period 3.Hotel List \n");
 	    	 System.out.println("Enter your choice : ");
 	    	 choice=sc.nextInt();
 	    	 
@@ -30,11 +33,55 @@ public class HotelMain {
 	    	    	hotelmain.addHotel();
 	    	    	break;
 	    	    	
+	    	    case 2 :
+	    	    	System.out.println("Enter the start date in (yyyy-mm-dd) : ");
+	    	    	String startDatestr=sc.next();
+	    	    	LocalDate startDate=LocalDate.parse(startDatestr, DateTimeFormatter.ISO_DATE);
+	    	    	
+	    	    	System.out.println("Enter the end date in (yyyy-mm-dd) : ");
+	    	    	String endDatestr=sc.next();
+	    	    	LocalDate endDate=LocalDate.parse(endDatestr, DateTimeFormatter.ISO_DATE);
+	    	    	
+	    	    	hotelmain.calculateHotelrate(startDate, endDate,hotelList);
+	    	    	break;
+	    	    	
+	    	    case 3: 
+	    	    	hotelmain.getAlllist();
+	    	    	break;
 	    	    default :
 	    	    	System.out.println("Invalid Choice...");
 	    	    	break;
 	    	 }
 	     }while(choice!=0);
+	}
+	public void calculateHotelrate(LocalDate startDate, LocalDate endDate, List<Hotel> hotelList) {
+	    Hotel cheapestHotel = hotelList.get(0);
+	    long lowestCost = Long.MAX_VALUE;
+	    long countDays = ChronoUnit.DAYS.between(startDate, endDate);
+//	    System.out.println("Days: " + countDays);
+
+	    for (int i = 0; i < hotelList.size(); i++) {
+	        Hotel hotel = hotelList.get(i);
+	        long totalCost = countDays * hotel.getPrice();
+	        if (totalCost < lowestCost) {
+	            lowestCost = totalCost;
+	            cheapestHotel = hotel;
+	        }
+//	        System.out.println("Total cost for " + hotel.getHotelName() + ": " + totalCost);
+	    }
+
+	    System.out.println("Cheapest Hotel Name is: " + cheapestHotel.getHotelName() + " and price is: " + lowestCost);
+	}
+
+	private void getAlllist() 
+	{
+		for (int i = 0; i < hotelList.size(); i++) 
+		{
+			Hotel hotel=hotelList.get(i);
+			System.out.println("Hotel name is : "+hotel.getHotelName());
+			System.out.println("Hotel price is : "+hotel.getPrice());
+			System.out.println("-----------------------------------------");
+		}
 	}
 	public void addHotel() 
 	{
@@ -53,7 +100,7 @@ public class HotelMain {
 		}
 		
 		hotelList.add(hotel);
-		System.out.println(hotelList);
+		System.out.println("Hotel added Sucessfully...");
 	}
 
 }
--- hotelReservationsystem1/src/test/java/com/java/hotelReservationsystem1/HotelReservationTestCase.java
+++ hotelReservationsystem1/src/test/java/com/java/hotelReservationsystem1/HotelReservationTestCase.java
@@ -2,20 +2,59 @@ package com.java.hotelReservationsystem1;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class HotelReservationTestCase {
 	Hotel hotel=new Hotel();
 	HotelMain hotelmain=new HotelMain();
 	
 	@Test
-	public void test() 
-	{
+	public void AddHotelTestCase() 
+	{	
 		String HotelName="vikas";
 		hotel.setHotelName(HotelName);
 		int price=200;
 		hotel.setPrice(price);
 		assertEquals(HotelName, hotel.getHotelName());
 		assertEquals(price, hotel.getPrice());
+	}
+	
+	@Test
+	public void calculateHotelrateTestCase() 
+	{
+		Hotel hotel1=new Hotel();
+		Hotel hotel2=new Hotel();
+		
+		String hotelName1="Lukewoood";
+		int price1=100;
+		hotel1.setHotelName(hotelName1);
+		hotel1.setPrice(price1);
+		
+		String hotelName2="David";
+		int price2=200;
+		hotel2.setHotelName(hotelName2);
+		hotel2.setPrice(price2);
+		
+		List<Hotel> hotelList=new ArrayList<Hotel>();
+		hotelList.add(hotel1);
+		hotelList.add(hotel2);
+		
+		LocalDate startDate=LocalDate.of(2024, 3, 1);
+		LocalDate endDate=LocalDate.of(2024, 4, 2);
+		
+		long countDays = ChronoUnit.DAYS.between(startDate, endDate);
+		long expectLowestcost=countDays * hotel1.getPrice();
+		 
+//		hotelmain.calculateHotelrate(startDate, endDate, hotelList);
+		
+		 assertEquals(hotelName1, hotel1.getHotelName());
+		 assertEquals(expectLowestcost, hotel1.getPrice() * countDays);
 	}
 }
